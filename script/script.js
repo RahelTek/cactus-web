f699437ce91ac566715afc6560307e93c1f128a1
RahelTek <rteklegiorgis@gmail.com>
2015-12-09 12:08:30 -0500

    h

--- script/script.js
+++ script/script.js
@@ -43,13 +43,3 @@ function ajaxForm(button){
       console.log(dataString); // Success actions go here.
     }
   });
-
-var numTimes = localStorage.getItem("visits-Hlfma");
-if(numTimes == null) {
-    numTimes = 0;
-} else {
-  numTimes = parseInt(numTimes, 10);
-}
-numTimes++;
-localStorage.setItem("visits-Hlfma", (numTimes).toString(10))
-document.getElementById("visit-times").textContent = numTimes.toString(10);
