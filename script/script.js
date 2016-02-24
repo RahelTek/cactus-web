72c54b6f4a72096898da2038cece8366b726cce2
RahelTek <rteklegiorgis@gmail.com>
2016-02-24 01:13:04 -0500

    remove line

--- script/script.js
+++ script/script.js
@@ -62,6 +62,7 @@ function ajaxForm(button){
     }
     document.getElementById("outlet").innerHTML = out; //adds all the content in the #out element
 }
+
 // Dropdown Menu
 var dropdown = document.querySelectorAll('.dropdown');
 var dropdownArray = Array.prototype.slice.call(dropdown,0);
